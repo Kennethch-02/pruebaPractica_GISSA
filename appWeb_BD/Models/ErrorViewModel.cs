c9e94456161beaadf51868a104e4c0fe3c7b35c4
Kenneth C <kennethalonsoc@gmail.com>
2023-02-09 15:26:35 -0600

    [Aplicación]: Creación de la estructura inicial de la aplicación

---
+++ appWeb_BD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appWeb_BD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
