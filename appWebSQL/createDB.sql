852b3184e991a8dd019c21efbc47508c77ab6f04
Kenneth C <kennethalonsoc@gmail.com>
2023-02-10 13:33:01 -0600

    [BackEnd]: Modificada la estructura de la base de datos

--- appWebSQL/createDB.sql
+++ appWebSQL/createDB.sql
@@ -4,133 +4,141 @@ BEGIN
 END;
 GO
 
+-- Usar la base de datos appWeb_BD
 USE appWeb_BD;
 GO
 
-/*Creacion de las tablas*/
-CREATE TABLE test_TipoUsuario (
-  TipoUsuarioID int IDENTITY(1,1) PRIMARY KEY,
-  NombreTipoUsuario varchar(50) NOT NULL
-);
-INSERT INTO test_TipoUsuario (NombreTipoUsuario)
-VALUES  ('Administrador'), 
-		('Consultor');
-
-CREATE TABLE test_TipoIdentificacion (
-  TipoIdentificacionID int IDENTITY(1,1) PRIMARY KEY,
-  NombreTipoIdentificacion varchar(50) NOT NULL
-);
-INSERT INTO test_TipoIdentificacion (NombreTipoIdentificacion)
-VALUES  ('Nacional'), 
-		('Extrangero');
-
-CREATE TABLE test_HabilidadesBlandas (
-  HabilidadBlandaID int IDENTITY(1,1) PRIMARY KEY,
-  NombreHabilidadBlanda varchar(50) NOT NULL
-);
-INSERT INTO test_HabilidadesBlandas (NombreHabilidadBlanda)
-VALUES  ('Buena comunicación'), 
-		('Buena organización'), 
-		('Trabajo en equipo'), 
-		('Puntualidad'), 
-		('Ser creativo'), 
-		('Facilidad de adaptación');
-
-CREATE TABLE test_Usuarios (
-  UsuarioID int IDENTITY(1,1) PRIMARY KEY,
-  TipoUsuarioID int NOT NULL FOREIGN KEY REFERENCES test_TipoUsuario(TipoUsuarioID),
-  TipoIdentificacionID int NOT NULL FOREIGN KEY REFERENCES test_TipoIdentificacion(TipoIdentificacionID),
-  NumeroIdentificacion varchar(50) NOT NULL,
-  NombreCompleto varchar(100) NOT NULL,
-  NombreUsuario varchar(50) NOT NULL,
-  Clave varchar(50) NOT NULL,
-  CorreoElectronico varchar(100) NOT NULL,
-  Telefono varchar(50) NOT NULL,
-  HabilidadBlandaID int NOT NULL FOREIGN KEY REFERENCES test_HabilidadesBlandas(HabilidadBlandaID)
+-- Crear la tabla test_usuarios
+CREATE TABLE test_usuarios (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  tipo_usuario VARCHAR(50) NOT NULL,
+  tipo_identificacion VARCHAR(50) NOT NULL,
+  num_identificacion VARCHAR(50) NOT NULL,
+  nombre_completo VARCHAR(255) NOT NULL,
+  nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
+  clave VARCHAR(500) NOT NULL,
+  correo_electronico VARCHAR(255) NOT NULL UNIQUE
 );
 GO
 
-/* Creacion de los procedimientos almacenados */
-CREATE PROCEDURE test_CrearUsuario (@TipoUsuarioID int, @TipoIdentificacionID int, @NumeroIdentificacion varchar(50), @NombreCompleto varchar(100), @NombreUsuario varchar(50), @Clave varchar(50), @CorreoElectronico varchar(100), @Telefono varchar(50), @HabilidadBlandaID int)
+-- Crear la tabla test_telefonos
+CREATE TABLE test_telefonos (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  telefono VARCHAR(50) NOT NULL,
+  usuario_id INT NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES test_usuarios(id)
+);
+GO
+
+-- Crear la tabla test_habilidades_blandas
+CREATE TABLE test_habilidades_blandas (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  habilidad VARCHAR(255) NOT NULL,
+  usuario_id INT NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES test_usuarios(id)
+);
+GO
+
+-- Funcion para separar String segun un caracter
+CREATE FUNCTION SplitString
+(    
+      @Input NVARCHAR(MAX),
+      @Character CHAR(1)
+)
+RETURNS @Output TABLE (
+      Item NVARCHAR(1000)
+)
 AS
 BEGIN
-  INSERT INTO test_Usuarios (TipoUsuarioID, TipoIdentificacionID, NumeroIdentificacion, NombreCompleto, NombreUsuario, Clave, CorreoElectronico, Telefono, HabilidadBlandaID)
-  VALUES (@TipoUsuarioID, @TipoIdentificacionID, @NumeroIdentificacion, @NombreCompleto, @NombreUsuario, @Clave, @CorreoElectronico, @Telefono, @HabilidadBlandaID)
+      DECLARE @StartIndex INT, @EndIndex INT
+      SET @StartIndex = 1
+      IF SUBSTRING(@Input, LEN(@Input) - 1, LEN(@Input)) <> @Character
+      BEGIN
+          SET @Input = @Input + @Character
+      END
+      WHILE CHARINDEX(@Character, @Input) > 0
+      BEGIN
+          SET @EndIndex = CHARINDEX(@Character, @Input)
+
+          INSERT INTO @Output(Item)
+          SELECT SUBSTRING(@Input, @StartIndex, @EndIndex - 1)
+
+          SET @Input = SUBSTRING(@Input, @EndIndex + 1, LEN(@Input))
+      END
+      RETURN
+END
+GO
+
+-- Procedimiento para agregar usuario
+CREATE PROCEDURE test_agregarUsuario (
+@tipo_usuario varchar(50), 
+@tipo_identificacion varchar(50), 
+@numero_identificacion varchar(50), 
+@nombre_completo varchar(100), 
+@nombre_usuario varchar(50), 
+@clave varchar(500), 
+@correo_electronico varchar(100), 
+@telefonos nvarchar(255), 
+@habilidades_blandas nvarchar(255))
+AS
+BEGIN
+  DECLARE @usuario_id INT;
+
+  INSERT INTO test_usuarios (tipo_usuario, tipo_identificacion, num_identificacion, nombre_completo, nombre_usuario, clave, correo_electronico)
+  VALUES (@tipo_usuario, @tipo_identificacion, @numero_identificacion, @nombre_completo, @nombre_usuario, @clave, @correo_electronico);
+
+  SET @usuario_id = SCOPE_IDENTITY();
+
+  DECLARE @telefonos_split TABLE (id INT IDENTITY(1,1), valor nvarchar(255));
+  DECLARE @habilidades_split TABLE (id INT IDENTITY(1,1), valor nvarchar(255));
+  DECLARE @telefono nvarchar(255);
+  DECLARE @habilidad nvarchar(255);
+
+  INSERT INTO @telefonos_split (valor) SELECT * FROM SplitString(@telefonos, ',');
+  INSERT INTO @habilidades_split (valor) SELECT * FROM SplitString(@habilidades_blandas, ',');
+
+  WHILE EXISTS (SELECT 1 FROM @telefonos_split)
+  BEGIN
+    SELECT TOP 1 @telefono = valor FROM @telefonos_split;
+    INSERT INTO test_telefonos (telefono, usuario_id) VALUES (@telefono, @usuario_id);
+    DELETE FROM @telefonos_split WHERE valor = @telefono;
+  END
+
+  WHILE EXISTS (SELECT 1 FROM @habilidades_split)
+  BEGIN
+    SELECT TOP 1 @habilidad = valor FROM @habilidades_split;
+    INSERT INTO test_habilidades_blandas (habilidad, usuario_id) VALUES (@habilidad, @usuario_id);
+    DELETE FROM @habilidades_split WHERE valor = @habilidad;
+  END
+END
+GO
+
+-- Procedimiento para verificar el Inicio de sesion
+ALTER PROCEDURE test_verificarInicioSesion (
+	@correo varchar(100),
+	@clave varchar(500), 
+	@resultado bit OUTPUT, 
+	@tipo_usuario varchar(50) OUTPUT)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @id INT;
+    DECLARE @clave_encriptada varchar(500);
+
+    SET @clave_encriptada = HASHBYTES('SHA2_512', @clave);
+
+    SELECT @id = id FROM test_usuarios 
+    WHERE correo_electronico = @correo AND clave = @clave_encriptada;
+
+    IF (@id IS NULL)
+        SET @resultado = 0;
+    ELSE
+    BEGIN
+        SET @resultado = 1;
+        SELECT @tipo_usuario = tipo_usuario FROM test_usuarios 
+        WHERE id = @id;
+    END;
 END;
 GO
-
-CREATE PROCEDURE test_LeerUsuario (@id INT)
-AS
-BEGIN
-  SELECT * FROM test_Usuarios WHERE UsuarioID = @id
-END
-GO
-
-CREATE PROCEDURE test_ActualizarUsuario (@id INT, @tipoUsuario VARCHAR(20), @tipoIdentificacion VARCHAR(20), @numIdentificacion VARCHAR(9),
-                                        @nombreCompleto VARCHAR(100), @username VARCHAR(50), @clave VARCHAR(50), @correoElectronico VARCHAR(100), @telefono VARCHAR(8))
-AS
-BEGIN
-  UPDATE test_Usuarios SET 
-	  TipoUsuarioID = @tipoUsuario, 
-	  TipoIdentificacionID = @tipoIdentificacion, 
-	  NumeroIdentificacion = @numIdentificacion,
-	  nombreCompleto = @nombreCompleto, 
-	  NombreUsuario = @username, 
-	  clave = @clave, 
-	  correoElectronico = @correoElectronico, 
-	  telefono = @telefono
-  WHERE UsuarioID = @id
-END
-
-GO
-CREATE PROCEDURE test_EliminarUsuario (@id INT)
-AS
-BEGIN
-  DELETE FROM test_Usuarios WHERE UsuarioID = @id
-END
-GO
-
-CREATE PROCEDURE test_ListarUsuarios
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT *
-    FROM test_Usuarios
-END
-GO
-
-CREATE PROCEDURE test_ListarHabilidades
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT *
-    FROM test_HabilidadesBlandas
-END
-GO
-
-CREATE PROCEDURE test_ListarTipoIdentificacion
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT *
-    FROM test_TipoIdentificacion
-END
-GO
-
-CREATE PROCEDURE test_ListarTipoUsuario
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT *
-    FROM test_TipoUsuario
-END
-GO
-
-CREATE PROCEDURE test_IniciarSesion (@CorreoElectronico varchar(100), @Clave nvarchar(50))
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT *
-    FROM test_Usuarios
-    WHERE correoElectronico = @CorreoElectronico AND Clave = @Clave
-END
+--
